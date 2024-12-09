8813c434f17643f7c7d7a48bc498aa004bf394df
A-Shermatov <azamatjon.shermatov@gmail.com>
2024-12-09 16:51:11 +0300

    example maps and final commit

--- app/src/main/java/com/example/collections/MainActivity.kt
+++ app/src/main/java/com/example/collections/MainActivity.kt
@@ -1,17 +1,5 @@
 package com.example.collections
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.collections.ui.theme.CollectionsTheme
 
 fun exampleArrays(){
     val rockPlanets = arrayOf("Mercury", "Venus", "Earth", "Mars")
@@ -77,8 +65,37 @@ fun exampleSets() {
 }
 
 
+fun exampleMaps() {
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
+    println(solarSystem.size)
+
+    solarSystem["Pluto"] = 5
+    println(solarSystem.size)
+
+    println(solarSystem["Pluto"])
+
+    // println(solarSystem.get("Theia"))
+
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"])
+}
+
+
 fun main(){
     exampleArrays()
     exampleLists()
     exampleSets()
+    exampleMaps()
 }
